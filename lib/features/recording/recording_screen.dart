37b22eed60af972ab31152b3f484e9ff8f57107e
Sultan <sultan.usman@yocket.in>
2025-12-03 11:47:11 +0530

    fix

--- lib/features/recording/recording_screen.dart
+++ lib/features/recording/recording_screen.dart
@@ -220,6 +220,7 @@ class _RecordingScreenState extends ConsumerState<RecordingScreen> {
                   children: [
                     // See uploaded chunks button
                     if (recordingState.session != null &&
+                        !recordingState.isRecording &&
                         (recordingState.totalChunks > 0 ||
                             recordingState.session!.totalChunks > 0))
                       Padding(
